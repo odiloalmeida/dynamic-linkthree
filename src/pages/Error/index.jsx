d2fede5ba2845678dad4a0d4938bc7b0f3a51fc0
Odilo <odilolima@hotmail.com>
2022-11-30 23:10:57 -0300

    added assets and change logo fix method

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -9,7 +9,7 @@ export default function Error(){
     return(
         <div className='error'>
             <Logo />
-            <h1>Página não encontrada</h1>
+            <h2>Página não encontrada</h2>
             <p>A página que voce procurou não existe</p>
             <Link className='link' to='/'>
                 Voltar a Home page
